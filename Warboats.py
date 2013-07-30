7f9f0ad715758b65656df8001626fb28d5f049ab
Deryl <arnol229@gmail.com>
2013-07-30 11:44:23 -0500

    Update Warboats.py
    Added comments, print and gen board functions

--- Warboats.py
+++ Warboats.py
@@ -1,33 +1,48 @@
 import random
 
 board = []
-
-def get_board(input):
-    input.lower()
-    if input == "create"
-        size = raw_input("please enter the dimensions (format #x#): ")
-        print "Generating your board with {0} rows and {1} collumns..."
-        row_size = int(size[0])
-        col_size = int(size[2])
-        
-        gen_board(row_size, col_size)
-    elif input == "random"
-        print "generating random board..."
-        row_size = randint(1,10)
-        col_size = randint(1,10)
-        gen_board(row_size, col_size)
-    else:
-        getboard(str(raw_input("error, please retry : Create / Random"))
-
-
-get_board(str(raw_input("please choose : Create / Random")))
-        
+# find acceptable spots to place ships
 def acceptable_spots(board):
     acc_spots = []
     for row in board:
         for spot in row:
             if spot != "1" and spot != "2":
-                shippiece1 = True
-                acc_spots.append([row][spot])
+                 shippiece1 = True
+                 acc_spots.append([row][spot])
+                 return acc_spots
 
-    return acc_spots
+# Find out if user wants to create or get random board
+def get_board(input):
+    input.lower()
+    if input == "create":
+        size = raw_input("please enter the dimensions (format #x#): ")
+        print "Generating your board with {} rows and {} collumns..."
+        row_size = int(size[0])
+        col_size = int(size[2])
+        print "Generating your board with {row_size} rows and {col_size} collumns..."
+        print row_size, col_size
+        gen_board(row_size, col_size)
+    elif input == "random":
+        print "generating random board..."
+        row_size = randint(1,10)
+        col_size = randint(1,10)
+        gen_board(row_size, col_size)
+        print row_size, col_size
+    else:
+        get_board(str(raw_input("error, please retry : Create / Random: ")))
+
+
+# generate the initial board
+def gen_board(rows, cols):
+    for row in range(rows):
+        board.append(["O"] * cols)
+        print_board(board)
+
+
+#Print board function
+def print_board(board):
+    for row in board:
+        for col in row:
+            print " ".join(row).format(col)
+        
+get_board(str(raw_input("please choose : Create / Random: ")))
