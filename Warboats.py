08d9cffd256c614294df0d564fead88fc89ac699
Deryl <arnol229@gmail.com>
2013-07-31 10:21:21 -0500

    trying to make acceptable spots...

--- Warboats.py
+++ Warboats.py
@@ -10,11 +10,12 @@ board = []
 def acceptable_spots(board):
     acc_spots = [] #acceptable spot list
     for row in board:
-        for spot in row:
-            if spot != "1" and spot != "2":
-                 shippiece1 = True
-                 acc_spots.append([row][spot])
-                 return acc_spots
+        for col in row:
+            while col == "O":
+                if col+1 == "O"
+                acc_spots.append([row][col],[row][col+1])
+
+    return acc_spots
 
 # Find out if user wants to create or get random board
 def get_board(input):
@@ -23,14 +24,13 @@ def get_board(input):
         size = raw_input("please enter the dimensions (format #x#): ")
         row_size = int(size[0])
         col_size = int(size[2])
-        #how do i get string concatenation working below?
         print "Generating your board with {} rows and {} collumns...".format(row_size, col_size)
         print row_size, col_size
         gen_board(row_size, col_size)
     elif input == "random":
         print "generating random board..."
-        row_size = randint(1,10)
-        col_size = randint(1,10)
+        row_size = randint(3,10)
+        col_size = randint(3,10)
         gen_board(row_size, col_size)
         print row_size, col_size
     else:
@@ -60,8 +60,3 @@ def print_board(board):
 # def place_ship():
         
 get_board(str(raw_input("please choose : Create / Random: ")))
-
-
-
-#LOL THIS IS A CHANGE SO THAT I CAN COMMIT AND PUSH
-#LOL - Why are we laughing? Is this a joke? I'm not sure... Am I even real? Or a figmant of code inside of the matrix? WHAT IS HAPPENING TO ME!?!?!
