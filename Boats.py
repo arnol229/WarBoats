be2127fbe42b05e79a67dc08b56e33883b1fba77
Robert Johnson <robrocker7@gmail.com>
2013-08-20 12:00:26 -0500

    example point check to see if alive

--- Boats.py
+++ Boats.py
@@ -1,14 +1,20 @@
 class Boat(object):
     self.Alive = True
+    self.points = []
 
 	__init__(self, Type, PositionList): #bring in type later? battleship,cruiser
 		for i in len(PositionList): # Create a ShipPoint for each point of length
-			self.Point[i] = ShipPoints(PositionList[i[0]], PositionList[i[1]])
+			self.points[i] = ShipPoints(PositionList[i[0]], PositionList[i[1]])
 			# ^^ will that work? creating point1, point2, point3 etc..?
 
 	def AliveOrDead(self):
 		# check to see if this boat's ship points are all dead
-        pass
+
+		for point in self.points:
+			if point.alive:
+				#return early because there is still 1 point alive
+				return False
+		return True
 
 class ShipPoints(object): #Each ship will be assigned 1 ShipPoint per point of its length
 	__init__(self, x, y):
