bb57e4d5e456fb79be1a98dc98e47618a81c23b0
Robert Johnson <robrocker7@gmail.com>
2013-08-20 12:05:31 -0500

    better loop

--- Boats.py
+++ Boats.py
@@ -3,9 +3,11 @@ class Boat(object):
     self.points = []
 
 	__init__(self, Type, PositionList): #bring in type later? battleship,cruiser
-		for i in len(PositionList): # Create a ShipPoint for each point of length
-			self.points[i] = ShipPoints(PositionList[i[0]], PositionList[i[1]])
-			# ^^ will that work? creating point1, point2, point3 etc..?
+		#for i in len(PositionList): # Create a ShipPoint for each point of length
+		#	self.points[i] = ShipPoints(PositionList[i[0]], PositionList[i[1]])
+		#	# ^^ will that work? creating point1, point2, point3 etc..?
+		for pl in PositionList:
+			self.points.append(pl)
 
 	def AliveOrDead(self):
 		# check to see if this boat's ship points are all dead
